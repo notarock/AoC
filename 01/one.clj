b39cd4e32c00a1cd893a44c421230c82afccbd3e
Roch D'Amour <roch.damour@gmail.com>
2020-12-02 01:07:02 -0500

    Day one star 2

--- 01/one.clj
+++ 01/one.clj
@@ -12,3 +12,20 @@
             (when (not= y 0)
               (recur (dec y))))))
       (recur (dec x)))))
+
+
+(let [input (str/split (slurp "input") #"\n")]
+  (loop [i (- (count input) 1)]
+    (when (and (not= i (count input)) (not= i 0))
+      (let [current (read-string (nth input i))]
+        (loop [x (- (count input) 1)]
+          (when (and (not= x (count input)) (not= x 0))
+            (loop [y (- (count input) 1)]
+              (let [first (read-string (nth input x))
+                    second (read-string (nth input y))]
+                (if (= (+ current first second) 2020)
+                  (println (* current first second))
+                  (when (not= y 0)
+                    (recur (dec y))))))
+            (recur (dec x)))))
+      (recur (dec i)))))
