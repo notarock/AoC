634224e40679ed0f5cb3d5961ffa301efc73c764
Roch D'Amour <roch.damour@gmail.com>
2020-12-02 23:16:11 -0500

    Part 2 solved

--- 02/solver.clj
+++ 02/solver.clj
@@ -1,6 +1,8 @@
 (ns solve
   (:require [clojure.string :as str]))
 
+;; Part 1
+
 (defn count-occurence [word letter]
   (->> word
        (re-seq (re-pattern letter))
@@ -22,3 +24,21 @@
 
 (let [input (str/split (slurp "input") #"\n")]
   (reduce + (map validate-password input)))
+
+;; Part 2
+
+(defn validate-second-password [password-line]
+  (let* [password (str/split password-line #" ")
+         pos1 (-> password (first) (str/split #"-") (first) (read-string))
+         pos2 (-> password (first) (str/split #"-") (second) (str/join) (read-string))
+         letter (-> password (second) (first) (str))
+         word (-> password (last) (str))]
+    (bit-xor (position-valid pos1 letter word) (position-valid pos2 letter word))))
+
+(defn position-valid [pos letter password]
+  (if (= (str (nth password (- pos 1))) letter)
+    1
+    0))
+
+(let [input (str/split (slurp "input") #"\n")]
+  (reduce + (map validate-second-password input)))
