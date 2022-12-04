7fd749fb3272c63a4c900dcd3344769ff5f9b97f
Roch D'Amour <roch.damour@gmail.com>
2022-12-04 18:57:13 -0500

    [2022] 01 part 2

--- 2022/01/main.go
+++ 2022/01/main.go
@@ -6,23 +6,57 @@ import (
 	"github.com/notarock/aoc/pkg/input"
 )
 
+type TopCalories struct {
+	one   int
+	two   int
+	three int
+}
+
+func (t TopCalories) Total() int {
+	return t.one + t.three + t.two
+}
+
+func (t *TopCalories) Update(current int) {
+	if current > t.one {
+		t.three = t.two
+		t.two = t.one
+		t.one = current
+		return
+	}
+
+	if current > t.two {
+		t.three = t.two
+		t.two = current
+		return
+	}
+
+	if current > t.three {
+		t.three = current
+		return
+	}
+}
+
 func main() {
-	// inputString := input.ReadInputAsString("./test.txt")
 	inputString := input.ReadInputAsString("./input.txt")
+	// inputString := input.ReadInputAsString("./input.txt")
 	current := 0
-	max := 0
-	for _, value := range inputString {
-		fmt.Println(value)
+	var max TopCalories
+	for i, value := range inputString {
 		if value == "" {
-			if max <= current {
-				fmt.Println(current, " sum value")
-				max = current
-			}
+			fmt.Println("Checking total of ", current)
+			max.Update(current)
 			current = 0
 		} else {
 			current = current + input.StrToInt(value)
 		}
+
+		if i == len(inputString)-1 {
+			fmt.Println("Checking total of ", current)
+			max.Update(current)
+			current = 0
+		}
 	}
 
 	fmt.Println("Maximum: ", max)
+	fmt.Println("Total: ", max.Total())
 }
